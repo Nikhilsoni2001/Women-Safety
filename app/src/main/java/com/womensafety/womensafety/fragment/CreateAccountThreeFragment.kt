6cee67488c103ec4fa36c25e6cf258ad884811fd
Nikhil soni <nsoni19012001@gmail.com>
2020-07-18 00:26:53 +0530

    feat: UI Added for OtpFragment.kt.

--- app/src/main/java/com/womensafety/womensafety/fragment/CreateAccountThreeFragment.kt
+++ app/src/main/java/com/womensafety/womensafety/fragment/CreateAccountThreeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.womensafety.womensafety.R
+import kotlinx.android.synthetic.main.fragment_create_account.view.*
 
 
 class  CreateAccountThreeFragment : Fragment() {
@@ -15,7 +16,10 @@ class  CreateAccountThreeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_create_account_three, container, false)
-
+        view.btnNext.setOnClickListener {
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.container, OtpFragment())?.commit()
+        }
         return view
     }
 }
--- app/src/main/java/com/womensafety/womensafety/fragment/OtpFragment.kt
+++ app/src/main/java/com/womensafety/womensafety/fragment/OtpFragment.kt
@@ -6,20 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.womensafety.womensafety.R
-import kotlinx.android.synthetic.main.fragment_create_account_two.view.btnNext
 
-class CreateAccountTwoFragment : Fragment() {
+class OtpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_create_account_two, container, false)
-
-        view.btnNext.setOnClickListener {
-            fragmentManager?.beginTransaction()
-                ?.replace(R.id.container, CreateAccountThreeFragment())?.commit()
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_otp, container, false)
     }
 }
