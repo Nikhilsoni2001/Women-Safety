1a9634a846cf1473a27f05e651579baee4c32728
agarg9582@gmail.com <agarg9582@gmail.com>
2020-07-30 00:20:24 +0530

    all constraints and changes

--- app/src/main/java/com/womensafety/womensafety/activity/HomeActivity.kt
+++ app/src/main/java/com/womensafety/womensafety/activity/HomeActivity.kt
@@ -4,40 +4,20 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.Style
 import com.womensafety.womensafety.R
 import com.womensafety.womensafety.util.SessionManager
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
-    private var mapView: MapView? = null
     private lateinit var sessionManager: SessionManager
     private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
-
         setContentView(R.layout.activity_home)
 
         sessionManager = SessionManager(this)
-        sharedPreferences = this.getSharedPreferences(sessionManager.PREF_NAME, sessionManager.PRIVATE_MODE)
-
-        mapView = findViewById(R.id.mapView)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
-
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-
-// Map is set up and the style has loaded. Now you can add data or make other map adjustments
-
-
-            }
-
-        }
-
+        sharedPreferences =
+            this.getSharedPreferences(sessionManager.PREF_NAME, sessionManager.PRIVATE_MODE)
         btnSignout.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             sessionManager.setLogin(false)
@@ -46,40 +26,5 @@ class HomeActivity : AppCompatActivity() {
             finish()
         }
 
-    }
-    // Add the mapView lifecycle to the activity's lifecycle methods
-    public override fun onResume() {
-        super.onResume()
-        mapView?.onResume()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView?.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView?.onStop()
-    }
-
-    public override fun onPause() {
-        super.onPause()
-        mapView?.onPause()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView?.onLowMemory()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView?.onDestroy()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
     }
 }
